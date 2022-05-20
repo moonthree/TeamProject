61a259c3ac776dfc60ed4d05c380486380bf4363
moonthree <fpdhslr0725@gmail.com>
2022-05-20 13:57:53 +0900

    0520 점심

---
+++ teamPjt/src/main/java/com/edu/vo/StoreReviewLikeVO.java
@@ -0,0 +1,28 @@
+package com.edu.vo;
+
+public class StoreReviewLikeVO {
+	private int store_review_like_idx;
+	private int store_review_idx;
+	private int member_idx;
+	public int getStore_review_like_idx() {
+		return store_review_like_idx;
+	}
+	public void setStore_review_like_idx(int store_review_like_idx) {
+		this.store_review_like_idx = store_review_like_idx;
+	}
+	public int getStore_review_idx() {
+		return store_review_idx;
+	}
+	public void setStore_review_idx(int store_review_idx) {
+		this.store_review_idx = store_review_idx;
+	}
+	public int getMember_idx() {
+		return member_idx;
+	}
+	public void setMember_idx(int member_idx) {
+		this.member_idx = member_idx;
+	}
+	
+	
+
+}
